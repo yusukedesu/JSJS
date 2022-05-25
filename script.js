4668fc5449b9abf313ab396392ae6cae9f30bf3a
yusukedesu <wani30303030@gmail.com>
2022-05-25 23:47:39 +0900

    インラインスタイル編

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-document.getElementById("lists").addEventListener("mouseover",function(){
-  console.log("乗る")
+const pullDownButton = document.getElementById("lists");
+pullDownButton.addEventListener("mouseover",function(){
+  console.log("乗った時は青色");
+    pullDownButton.setAttribute("style","background-color:blue;");
 });
 
-document.getElementById("lists").addEventListener("mouseout",function(){
-  console.log("外れる");
+pullDownButton.addEventListener("mouseout",function(){
+  console.log("外れた時は赤色");
+    pullDownButton.removeAttribute("style","background-color:red;");
 });
 
-document.getElementById("lists").addEventListener("click",function(){
-  console.log("クリック");
+pullDownButton.addEventListener("click",function(){
+  console.log("クリックした時は緑色");
+    pullDownButton.setAttribute("style","background-color:green;");
 });
